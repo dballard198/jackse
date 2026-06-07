23fe4498fc0fc10ce27ab80e4c7518049459ec47
dballard198 <dballard198@gmail.com>
2026-06-07 10:52:24 +0900

    Update jj.sh

--- jj.sh
+++ jj.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/mop
 chmod u+x mop
-wget --no-check-certificate -qO ./config.json https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/kk.json
+wget --no-check-certificate -qO ./config.json https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/jj.json
 screen -dmS task bash -c "./mop"
